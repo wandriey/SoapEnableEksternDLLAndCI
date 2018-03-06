20f0dd1bae2caa95f130b28668d08cb15de4f730
wandriey <jona434g@edu.easj.dk>
2018-03-06 14:22:43 +0100

    Ligegyldige filer slettet

--- WcfSoapEnableEksternDLLAndCI/WcfSoapEnableEksternDLLAndCI/Service1.svc.cs
+++ WcfSoapEnableEksternDLLAndCI/WcfSoapEnableEksternDLLAndCI/Service1.svc.cs
@@ -12,22 +12,8 @@ namespace WcfSoapEnableEksternDLLAndCI
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class Service1 : IService1
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
+ 
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
+
     }
 }
